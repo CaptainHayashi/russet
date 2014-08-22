1f447ff815b77d85ecf2e03c332b98da32c4a57e
Matt Windsor <mattwindsor@btinternet.com>
2014-08-22 18:21:55 +0000

    Add shell-style tokeniser tests.

--- src/builders/shell_style.rs
+++ src/builders/shell_style.rs
@@ -41,3 +41,87 @@ pub fn shell_style_tokeniser() -> StockTokeniser {
         vec![ ( '\\', LiteralEscape ) ].move_iter().collect();
     Tokeniser::new(quote_map, escape_map)
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::shell_style_tokeniser;
+    use line::LineTokeniser;
+    use tokeniser::{ UnmatchedQuote, UnfinishedEscape };
+
+    #[test]
+    fn shell_style_unmatched_single_quote() {
+        assert_eq!(shell_style_tokeniser.line("\'abcde"),
+                   Err(UnmatchedQuote));
+    }
+
+    #[test]
+    fn shell_style_unmatched_double_quote() {
+        assert_eq!(shell_style_tokeniser.line("\"abcde"),
+                   Err(UnmatchedQuote));
+    }
+
+    #[test]
+    fn shell_style_unfinished_escape() {
+        assert_eq!(shell_style_tokeniser.line("zxcvbn m\\"),
+                   Err(UnfinishedEscape));
+    }
+
+    #[test]
+    fn shell_style_empty_string() {
+        assert_eq!(shell_style_tokeniser.line(""), Ok(vec![]));
+    }
+
+    #[test]
+    fn shell_style_leading_whitespace() {
+        let rhs = vec![ "abc".into_string(), "def".into_string() ];
+        assert_eq!(shell_style_tokeniser.line("     abc def"), Ok(rhs));
+    }
+
+    #[test]
+    fn shell_style_trailing_whitespace() {
+        let rhs = vec![ "ghi".into_string(), "jkl".into_string() ];
+        assert_eq!(shell_style_tokeniser.line("ghi jkl     \n"), Ok(rhs));
+    }
+
+    #[test]
+    fn shell_style_enqueue_command_double_quotes() {
+        let lhs =
+            "enqueue file \"C:\\\\Users\\\\Test\\\\Artist - Title.mp3\" 1";
+        let rhs = vec![ "enqueue".into_string(),
+                        "file".into_string(),
+                        "C:\\Users\\Test\\Artist - Title.mp3".into_string(),
+                        "1".into_string() ];
+        assert_eq!(shell_style_tokeniser.line(lhs), Ok(rhs));
+    }
+
+    #[test]
+    fn shell_style_enqueue_command_single_quotes() {
+        let lhs =
+            "enqueue file \'C:\\Users\\Test\\Artist - Title.mp3\' 1";
+        let rhs = vec![ "enqueue".into_string(),
+                        "file".into_string(),
+                        "C:\\Users\\Test\\Artist - Title.mp3".into_string(),
+                        "1".into_string() ];
+        assert_eq!(shell_style_tokeniser.line(lhs), Ok(rhs));
+    }
+
+    #[test]
+    fn shell_style_escaped_newline() {
+        // Unquoted backslash-escape
+        assert_eq!(shell_style_tokeniser.line("abc\\\nde"),
+                   Ok(vec![ "abc\nde".into_string() ]));
+        // Double-quoted backslash-escape
+        assert_eq!(shell_style_tokeniser.line("\"abc\\\nde\""),
+                   Ok(vec![ "abc\nde".into_string() ]));
+        // Double-quoted implicit-escape
+        assert_eq!(shell_style_tokeniser.line("\"abc\nde\""),
+                   Ok(vec![ "abc\nde".into_string() ]));
+        // Single-quoted implicit-escape
+        assert_eq!(shell_style_tokeniser.line("\'abc\nde\'"),
+                   Ok(vec![ "abc\nde".into_string() ]));
+        // But single-quoted backslash-escape shouldn't work
+        assert_eq!(shell_style_tokeniser.line("\'abc\\\nde\'"),
+                   Ok(vec![ "abc\\\nde".into_string() ]));
+    }
+}
